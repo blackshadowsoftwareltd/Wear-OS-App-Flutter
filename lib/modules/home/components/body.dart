47185746c93594f081c301a65005a8a2345b5b2e
blackshadowsoftwareltd <blackshadowsoftwareltd@gmail.com>
2023-05-06 00:53:04 +0600

    stream the rotar

--- lib/modules/home/components/body.dart
+++ lib/modules/home/components/body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'
-    show ConsumerWidget, WidgetRef;
+    show Consumer, ConsumerWidget, WidgetRef;
 
 import '../../run/providers/provider.dart'
-    show ambientMoodProvider, deviceShapeProvider;
+    show ambientMoodProvider, deviceShapeProvider, rotarProvider;
 
 class HomeBody extends ConsumerWidget {
   const HomeBody({super.key});
@@ -12,10 +12,21 @@ class HomeBody extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final shape = ref.watch(deviceShapeProvider);
     final mood = ref.watch(ambientMoodProvider);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [Text(shape.name), Text(mood.name)],
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Shape : ${shape.name}'),
+          Text('Ambient : ${mood.name}'),
+          Consumer(builder: (context, state, __) {
+            final rotar = ref.watch(rotarProvider);
+
+            return Text('Rotar : $rotar');
+          })
+        ],
+      ),
     );
   }
 }
--- lib/modules/run/providers/provider.dart
+++ lib/modules/run/providers/provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart'
-    show Notifier, NotifierProvider;
+    show Notifier, NotifierProvider, StreamProvider;
 import 'package:wear/wear.dart' show WearMode, WearShape;
+import 'package:wearable_rotary/wearable_rotary.dart';
 
 final deviceShapeProvider =
     NotifierProvider<_DeviceShape, WearShape>(_DeviceShape.new);
@@ -22,4 +23,27 @@ class _AmbientMood extends Notifier<WearMode> {
 
   Future<void> update(WearMode mood) async =>
       await Future.delayed(Duration.zero, () => state = mood);
+}
+
+final rotarStream = StreamProvider<RotaryEvent>((ref) => rotaryEvents);
+
+final rotarProvider = NotifierProvider<_Rotar, int>(_Rotar.new);
+
+class _Rotar extends Notifier<int> {
+  int r = 0;
+  @override
+  int build() {
+    final rs = ref.watch(rotarStream).value;
+    return update(rs);
+  }
+
+  int update(RotaryEvent? e) {
+    if (e == null) return 0;
+    if (e.direction == RotaryDirection.clockwise) {
+      if (r < 100) r++;
+    } else {
+      if (r > 0) r--;
+    }
+    return r;
+  }
 }
